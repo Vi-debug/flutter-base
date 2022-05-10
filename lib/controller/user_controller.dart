85c457e1284ef37a41cd29f33a4725b7f82e2a99
Ky Anh <anh.nguyenky@petronas.com>
2022-05-10 18:16:45 +0700

    add loading, handle error

--- lib/controller/user_controller.dart
+++ lib/controller/user_controller.dart
@@ -1,26 +1,29 @@
-import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_example/repository/user_repository.dart';
 
 import '../models/user.dart';
 
+final listUserProvider =
+    StateNotifierProvider<UserController, AsyncValue<List<User>>>((ref) {
+  return UserController();
+});
 
-class UserController extends StateNotifier<List<User>> {
-  
+class UserController extends StateNotifier<AsyncValue<List<User>>> {
   final UserRepository userRepository = UserRepository();
 
-  UserController() : super([]);
-  
-  void getAllUsers() async {
-    final newList = await userRepository.getAllUsers();
-    state = newList;
+  UserController() : super(const AsyncValue.data([]));
+
+  Future<void> getAllUsers() async {
+    try {
+      state = const AsyncValue.loading();
+      final newList = await userRepository.getAllUsers();
+      state = AsyncValue.data(newList);
+    } catch (e) {
+      state = AsyncValue.error(e);
+    }
   }
 
   void removeAllUser() {
-    state = [];
+    state = const AsyncValue.data([]);
   }
 }
-
-final listUserProvider = StateNotifierProvider<UserController, List<User>>((ref) {
-  return UserController();
-});
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_example/base/base_error.dart';
 
 import 'controller/user_controller.dart';
 import 'models/user.dart';
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -36,51 +38,88 @@ class MyHomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final List<User> listUsers = ref.watch(listUserProvider);
-    ref.listen(listUserProvider, ((previous, next) {
-      
-    }));
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Riverpod demo'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: ListView.builder(
-            itemCount: listUsers.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Text(listUsers[index].gender),
-                    const SizedBox(width: 20),
-                    Text(listUsers[index].email),
-                  ],
+    final AsyncValue<List<User>> listUserAsyncValue =
+        ref.watch(listUserProvider);
+
+    ref.listen<AsyncValue<List<User>>>(
+        listUserProvider, (e, state) => state.showSnackBarOnError(context));
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            title: const Text('Riverpod demo'),
+          ),
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: listUserAsyncValue.when(
+                data: (listUser) => ListView.builder(
+                  itemCount: listUser.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(listUser[index].gender),
+                          const SizedBox(width: 20),
+                          Text(listUser[index].email),
+                        ],
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
+                error: (_, __) => null,
+                loading: () => null,
+              ),
+            ),
+          ),
+          floatingActionButton: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  ref.read(listUserProvider.notifier).getAllUsers();
+                },
+                child: const Icon(Icons.add),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  ref.read(listUserProvider.notifier).removeAllUser();
+                },
+                child: const Icon(Icons.restore_from_trash),
+              )
+            ],
           ),
         ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              ref.read(listUserProvider.notifier).getAllUsers();
-            },
-            child: const Icon(Icons.add),
+        Visibility(
+          visible: listUserAsyncValue.isLoading,
+          child: Positioned.fill(
+            child: Container(
+              color: Colors.black26,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  CircularProgressIndicator(),
+                ],
+              ),
+            ),
           ),
-          FloatingActionButton(
-            onPressed: () {
-              ref.read(listUserProvider.notifier).removeAllUser();
-            },
-            child: const Icon(Icons.restore_from_trash),
-          )
-        ],
-      ),
+        ),
+      ],
     );
   }
+}
+
+extension on AsyncValue {
+  // isLoading shorthand (AsyncLoading is a subclass of AsycValue)
+  bool get isLoading => this is AsyncLoading;
+
+  // show a snackbar on error only
+  void showSnackBarOnError(BuildContext context) => whenOrNull(
+        error: (error, _) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(BaseError.fromError(error, _).message)),
+          );
+        },
+      );
 }
